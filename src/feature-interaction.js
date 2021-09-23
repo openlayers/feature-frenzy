77674b975dbceb9560e29f3a73783f759161495b
Andreas Hocevar <andreas.hocevar@gmail.com>
2021-09-23 23:35:54 +0000

    Features interaction example

---
+++ src/feature-interaction.js
@@ -0,0 +1,56 @@
+import MapboxVectorLayer from 'ol/layer/MapboxVector';
+import {Map, View} from 'ol';
+import {Stroke, Style} from 'ol/style';
+import {VectorTile} from 'ol/layer';
+import {fromLonLat} from 'ol/proj';
+import {recordStyleLayer} from 'ol-mapbox-style/dist/stylefunction';
+
+recordStyleLayer(true);
+
+let selected = [];
+const style = new Style({
+  stroke: new Stroke({
+    color: 'rgba(0, 0, 0, 0.8)',
+    width: 1.5,
+  }),
+});
+const selection = new VectorTile({
+  properties: {
+    selected: [],
+  },
+  style: (feature) =>
+    selected.includes(feature.get('bofo_id')) ? style : undefined,
+});
+
+const info = document.getElementById('output');
+function showInfo(features) {
+  selected = features.map((feature) => feature.get('bofo_id'));
+  selection.changed();
+  if (features.length == 0) {
+    info.innerText = '';
+    info.style.opacity = 0;
+    return;
+  }
+  const ids = features.map((feature) => feature.get('mapbox-layer').id);
+  info.innerText = ids.join(',');
+  info.style.opacity = 1;
+}
+
+const layer = new MapboxVectorLayer({
+  styleUrl: 'https://bodenkarte.at/styles/typengruppe.json',
+});
+
+layer.on('change:source', () => selection.setSource(layer.getSource()));
+
+const map = new Map({
+  target: 'map-container',
+  layers: [layer, selection],
+  view: new View({
+    center: fromLonLat([15.51, 47.28]),
+    zoom: 14,
+  }),
+});
+
+map.on('pointermove', (event) => {
+  layer.getFeatures(event.pixel).then(showInfo);
+});
