ba59abb8b5ca0d5d316472a8f47d3676c095bb90
Tim Schaub <tim.schaub@gmail.com>
2021-09-23 21:24:23 +0000

    Editing slides

--- src/feature-editor.js
+++ src/feature-editor.js
@@ -19,38 +19,24 @@ const source = new VectorSource({
   url: dataURL,
 });
 
-const overlay = new TileLayer({
-  source: new XYZSource({
-    url: 'https://api.mapbox.com/styles/v1/tschaub/cjh953yhw1b2l2rqwpbglpnxq/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoidHNjaGF1YiIsImEiOiJjaW5zYW5lNHkxMTNmdWttM3JyOHZtMmNtIn0.CDIBD8H-G2Gf-cPkIuWtRg',
-  }),
-});
-
 const map = new Map({
   target: 'map-container',
   layers: [
     new TileLayer({
       source: new XYZSource({
-        url: 'https://api.mapbox.com/styles/v1/tschaub/cjh7585xo2lcf2soz7wkbgkud/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoidHNjaGF1YiIsImEiOiJjaW5zYW5lNHkxMTNmdWttM3JyOHZtMmNtIn0.CDIBD8H-G2Gf-cPkIuWtRg',
+        crossOrigin: 'anonymous',
+        url: 'https://api.maptiler.com/tiles/satellite/{z}/{x}/{y}.jpg?key=get_your_own_D6rA4zTHduk6KOKTXzGB',
       }),
     }),
     new VectorLayer({
       source: source,
-      style: [
-        new Style({
-          stroke: new Stroke({
-            color: [255, 255, 255, 0.5],
-            width: 6,
-          }),
+      style: new Style({
+        stroke: new Stroke({
+          color: [0, 153, 255, 1],
+          width: 2,
         }),
-        new Style({
-          stroke: new Stroke({
-            color: [0, 153, 255, 1],
-            width: 2,
-          }),
-        }),
-      ],
+      }),
     }),
-    overlay,
   ],
   view: new View({
     center: fromLonLat([-79.093, 35.224]),
@@ -65,34 +51,5 @@ const snap = new Snap({source: source});
 map.addInteraction(modify);
 map.addInteraction(draw);
 map.addInteraction(snap);
-
-document.addEventListener('keydown', function (event) {
-  if (event.which === 32) {
-    event.preventDefault();
-    setEditing(true);
-    return false;
-  }
-  if (event.which === 27) {
-    event.preventDefault();
-    setEditing(false);
-    return false;
-  }
-});
-
-const instructions = document.getElementById('instructions');
-function setEditing(editing) {
-  overlay.setVisible(!editing);
-  modify.setActive(editing);
-  draw.setActive(editing);
-  snap.setActive(editing);
-  let markup;
-  if (editing) {
-    markup = 'press <code>&lt;esc&gt;</code> to stop editing';
-  } else {
-    markup = 'press <code>&lt;space&gt;</code> to start editing';
-  }
-  instructions.innerHTML = markup;
-}
-setEditing(false);
 
 sync(map);
