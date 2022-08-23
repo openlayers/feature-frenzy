12226c95978fb287a4bde89a1e52c7ccebd565d1
Tim Schaub <tim.schaub@gmail.com>
2022-08-23 16:33:37 +0200

    Move NDVI into cog viewer and list additional enhancemnets

--- src/cog-viewer.js
+++ src/cog-viewer.js
@@ -4,7 +4,7 @@ import TileLayer from 'ol/layer/WebGLTile.js';
 
 const images = [
   {
-    name: 'Florence',
+    name: 'Firenze',
     base: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/32/T/PP/2022/7/S2B_32TPP_20220722_0_L2A',
   },
   {
@@ -34,6 +34,67 @@ const visualizations = [
         ['/', ['band', 2], 5000],
         ['/', ['band', 3], 5000],
         1,
+      ],
+    },
+  },
+  {
+    name: 'NDVI',
+    sources: ['B04', 'B08'],
+    normalize: false,
+    display: (element, data) => {
+      if (!data) {
+        return;
+      }
+      const ndvi = (data[1] - data[0]) / (data[1] + data[0]);
+      element.textContent = `NDVI: ${spacePad(ndvi.toFixed(2), 5)}`;
+    },
+    style: {
+      color: [
+        'interpolate',
+        ['linear'],
+        // calculate NDVI, bands come from the sources below
+        ['/', ['-', ['band', 2], ['band', 1]], ['+', ['band', 2], ['band', 1]]],
+        // color ramp for NDVI values, ranging from -1 to 1
+        -0.2,
+        [191, 191, 191],
+        -0.1,
+        [219, 219, 219],
+        0,
+        [255, 255, 224],
+        0.025,
+        [255, 250, 204],
+        0.05,
+        [237, 232, 181],
+        0.075,
+        [222, 217, 156],
+        0.1,
+        [204, 199, 130],
+        0.125,
+        [189, 184, 107],
+        0.15,
+        [176, 194, 97],
+        0.175,
+        [163, 204, 89],
+        0.2,
+        [145, 191, 82],
+        0.25,
+        [128, 179, 71],
+        0.3,
+        [112, 163, 64],
+        0.35,
+        [97, 150, 54],
+        0.4,
+        [79, 138, 46],
+        0.45,
+        [64, 125, 36],
+        0.5,
+        [48, 110, 28],
+        0.55,
+        [33, 97, 18],
+        0.6,
+        [15, 84, 10],
+        0.65,
+        [0, 69, 0],
       ],
     },
   },
@@ -112,6 +173,12 @@ function displayPixelValue(event) {
   if (!data) {
     return;
   }
+  const visualization = visualizations[visualizationSelector.selectedIndex];
+  if (visualization.display) {
+    visualization.display(info, data);
+    return;
+  }
+
   const lines = Array.from(data)
     .slice(0, 3)
     .map((value, i) => `B${i + 1}: ${spacePad(value, 5)}  `);
