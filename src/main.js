df1160beba4bf12597eef3a8000a92ed87d338a4
Andreas Hocevar <andreas.hocevar@gmail.com>
2022-08-23 22:48:03 +0200

    Mouse navigation: right half of the sreeen -> forward, left half -> back

--- src/main.js
+++ src/main.js
@@ -6,3 +6,11 @@ import javascript from 'highlight.js/lib/languages/javascript';
 hljs.registerLanguage('javascript', javascript);
 hljs.highlightAll();
 // hljs.initHighlightingOnLoad();
+
+document.body.addEventListener('click', (e) => {
+  if (e.clientX > document.body.clientWidth / 2) {
+    globalThis.big.forward();
+  } else {
+    globalThis.big.go(big.current - 1);
+  }
+});
