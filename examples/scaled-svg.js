12681c230a9f918c0a17142ea77ea5ff546f4bbd
Andreas Hocevar <andreas.hocevar@gmail.com>
2024-12-05 10:06:34 -0300

    Add decode/load buttons to the scaled-svg example

--- examples/scaled-svg.js
+++ examples/scaled-svg.js
@@ -3,28 +3,36 @@ import Map from 'ol/Map.js';
 import Source from 'ol/source/Image.js';
 import View from 'ol/View.js';
 import {createLoader as createStatic} from 'ol/source/static.js';
-import {load as loadImage} from 'ol/Image';
+import {decode as decodeImage, load as loadImage} from 'ol/Image';
 import {useGeographic} from 'ol/proj.js';
 
 useGeographic();
 
-const source = new Source({
-  loader: createStatic({
-    url: '../wms.svg',
-    imageExtent: [-20037508.34, -20037508.34, 20037508.34, 20037508.34],
-    load: loadImage,
-  }),
-});
+const layer = new Layer();
+const changeSource = (load) => {
+  const source = new Source({
+    loader: createStatic({
+      url: '../wms.svg',
+      imageExtent: [-20037508.34, -20037508.34, 20037508.34, 20037508.34],
+      load,
+    }),
+  });
+  layer.setSource(source);
+};
+changeSource(loadImage);
 
 const map = new Map({
   target: 'map',
-  layers: [
-    new Layer({
-      source,
-    }),
-  ],
+  layers: [layer],
   view: new View({
     center: [-58, 0],
     zoom: 5,
   }),
+});
+
+document.getElementById('decode').addEventListener('click', function () {
+  changeSource(decodeImage);
+});
+document.getElementById('load').addEventListener('click', function () {
+  changeSource(loadImage);
 });
